72ec27a18bf6000209ae23bb3cb6bde3edbc62bb
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-09-14 13:32:33 +0200

    Add project files.

---
+++ H1_Library/Controller/MainController.cs
@@ -0,0 +1,34 @@
+﻿using H1_Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Library.Controller
+{
+    internal class MainController
+    {
+        public void Start()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Book> CreateNewLoanList()
+        {
+            Book bog1 = new Book("Anders And", 102);
+            Book bog2 = new Book("Hvalen Umut", 35);
+            Book bog3 = new Book("Aben Andreas", 243);
+
+            List<Book> booksAvailable = new List<Book>()
+            {
+                bog1, bog2, bog3
+            };
+
+            return booksAvailable;
+        }
+    }
+}
---
+++ H1_Library/Model/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_Library.Model
+{
+    /// <summary>
+    /// Class <see cref="Book"/> represents a book
+    /// </summary>
+    internal class Book
+    {
+        private string _title;
+        private ushort _pageAmount;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Book"/> class, using multiple arguments
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="pageAmount"></param>
+        public Book(string title, ushort pageAmount)
+        {
+            _title = title;
+            _pageAmount = pageAmount;
+        }
+
+        public string Title
+        {
+            get { return _title; }
+        }
+    }
+}
